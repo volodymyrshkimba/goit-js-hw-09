fc2bf089d24fa9aac7a60a0e8f90f2e868aeb72a
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-07-31 15:46:23 +0300

    start form task

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,35 @@
-console.log('hello');
+const formData = {
+	email : '',
+	message : '',
+}
+
+const key = "feedback-form-state";
+const formEl = document.querySelector('.feedback-form')
+
+if (localStorage.getItem(key)) {
+formEl.elements.email.value = JSON.parse(localStorage.getItem(key)).email;
+formEl.elements.message.value = JSON.parse(localStorage.getItem(key)).message;
+formData.email = JSON.parse(localStorage.getItem(key)).email;
+formData.message = JSON.parse(localStorage.getItem(key)).message;
+}
+
+const onFormElInput = () => {
+	formData.email = formEl.elements.email.value.trim();
+	formData.message = formEl.elements.message.value.trim();
+   localStorage.setItem(key, JSON.stringify(formData));
+}
+
+const onFormElSubmit = event => {
+	event.preventDefault();
+	if (formData.email === '' || formData.message === '') {
+		alert('Fill please all fields');
+		return;
+	}
+	console.log(formData);
+	localStorage.removeItem(key);
+	formEl.reset();
+}
+
+formEl.addEventListener('input', onFormElInput)
+formEl.addEventListener('submit', onFormElSubmit)
+
